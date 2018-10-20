decc85ca2d020209b0c0c15bd9466a96b102c1cc
Vinayak Mehta <vmehta94@gmail.com>
2018-10-20 22:03:50 +0530

    Add doc templates and Makefile

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -48,8 +48,6 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
 source_suffix = '.rst'
 
 # The master toctree document.
@@ -75,14 +73,19 @@ pygments_style = None
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+html_theme_options = {
+    'show_powered_by': False,
+    'github_user': 'camelot-dev',
+    'github_repo': 'excalibur',
+    'github_banner': True,
+    'show_related': False,
+    'note_bg': '#FFF59C'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -96,14 +99,18 @@ html_static_path = ['_static']
 # defined by theme itself.  Builtin themes are using these templates by
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
-#
-# html_sidebars = {}
+html_sidebars = {
+    'index': ['sidebarintro.html', 'relations.html', 'sourcelink.html',
+              'searchbox.html', 'hacks.html'],
+    '**': ['sidebarlogo.html', 'localtoc.html', 'relations.html',
+           'sourcelink.html', 'searchbox.html', 'hacks.html']
+}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'excaliburdoc'
+htmlhelp_basename = 'Excaliburdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -130,7 +137,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'excalibur.tex', 'excalibur Documentation',
+    (master_doc, 'Excalibur.tex', 'Excalibur Documentation',
      'Camelot Developers', 'manual'),
 ]
 
@@ -140,7 +147,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'excalibur', 'excalibur Documentation',
+    (master_doc, 'Excalibur', 'Excalibur Documentation',
      [author], 1)
 ]
 
@@ -151,8 +158,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'excalibur', 'excalibur Documentation',
-     author, 'excalibur', 'One line description of project.',
+    (master_doc, 'Excalibur', 'Excalibur Documentation',
+     author, 'Excalibur', 'One line description of project.',
      'Miscellaneous'),
 ]
 
