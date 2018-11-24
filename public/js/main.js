e04e552faa769830e48dc65da143d47879949548
Vinayak Mehta <vmehta94@gmail.com>
2018-11-24 07:15:42 +0530

    Add fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  var clipboard = new ClipboardJS(".-js-copy-pip-command");
+  clipboard.on("success", function(e) {
+    e.trigger.setAttribute("aria-label", "Copied!");
+    e.clearSelection();
+  });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
